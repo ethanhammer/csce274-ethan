8e768903df541bcbe65511917f016704a8b5c83b
Ethan Hammer <ethanhammer2217@gmail.com>
2024-09-12 19:17:05 -0400

    new

--- packages/homework3/src/publisher.py
+++ packages/homework3/src/publisher.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Int32  
+from std_msgs.msg import Float32  # Import Float32 instead of Int32
 
 def talker():
-    
-    pub = rospy.Publisher('delta', Int32, queue_size=10)
+    # Create a publisher for Float32 messages
+    pub = rospy.Publisher('delta', Float32, queue_size=10)
     
     # Initialize the ROS node
     rospy.init_node('publisher', anonymous=True)
@@ -12,11 +12,10 @@ def talker():
     rate = rospy.Rate(10)  # 10hz
     
     while not rospy.is_shutdown():
-
-        msg = Int32()
-        msg.data = 10  # Set the message data
+        msg = Float32()  # Create a Float32 message
+        msg.data = 10.0  # Set the message data as a float
         
-        pub.publish(msg)
+        pub.publish(msg)  # Publish the message
         
         rate.sleep()
 
--- packages/homework3/src/subsubscriber.py
+++ packages/homework3/src/subsubscriber.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32  # Import Float32 instead of Int32
 
 def callback(msg):
     # This function will be called whenever a new message is received
-    rospy.loginfo("Received: %d", msg.data)
+    rospy.loginfo("Received: %f", msg.data)  # Log message data as a float
 
 def listener():
     # Initialize the ROS node
     rospy.init_node('subscriber_node', anonymous=True)
     
-    # Create a subscriber to the "total" topic with std_msgs/Int32 messages
-    rospy.Subscriber('total', Int32, callback)
+    # Create a subscriber to the "total" topic with std_msgs/Float32 messages
+    rospy.Subscriber('total', Float32, callback)
     
     # Keep the node running to process incoming messages
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
+
