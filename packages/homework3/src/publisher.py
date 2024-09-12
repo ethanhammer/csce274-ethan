6900f7f9520fa01af6d942fba476d9efdaae61f2
Ethan Hammer <ethanhammer2217@gmail.com>
2024-09-12 17:11:41 -0400

    publish

---
+++ packages/homework3/src/publisher.py
@@ -0,0 +1,28 @@
+
+import rospy
+from std_msgs.msg import Int32  
+
+def talker():
+    
+    pub = rospy.Publisher('delta', Int32, queue_size=10)
+    
+    # Initialize the ROS node
+    rospy.init_node('publisher', anonymous=True)
+    
+    rate = rospy.Rate(10)  # 10hz
+    
+    while not rospy.is_shutdown():
+
+        msg = Int32()
+        msg.data = 10  # Set the message data
+        
+        pub.publish(msg)
+        
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
