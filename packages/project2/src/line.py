68400901509849927daea7832ea96a932c69ddcf
Ethan Hammer <ethanhammer2217@gmail.com>
2024-09-29 19:38:43 -0400

    new

--- packages/project2/src/line.py
+++ packages/project2/src/line.py
@@ -1,41 +1,58 @@
 #!/usr/bin/env python3
 
 import rospy
-from duckietown_msgs.msg import Twist2DStamped  # Adjust package name as necessary
-import time
+from duckietown_msgs.msg import Twist2DStamped
 
 def move_duckiebot():
-    rospy.init_node('line', anonymous=True)
+    rospy.init_node('square_mover', anonymous=True)
     pub = rospy.Publisher('/bigbot/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
 
-    # Parameters
-    distance = 1.0  # 1 meter
-    speed = 0.3     # m/s
-    time_to_move = distance / speed  # time in seconds
 
-    # Create a Twist2DStamped message for forward movement
+    distance = 1.0  
+    speed = 0.3     
+    time_to_move = distance / speed  
+    turn_speed = 0.5  
+    turn_time = 1.57 / turn_speed  
+
     move_command = Twist2DStamped()
     move_command.header.stamp = rospy.Time.now()
-    move_command.v = speed  # Set linear velocity
-    move_command.omega = 0  # Set angular velocity
+    move_command.v = speed
+    move_command.omega = 0
 
-    # Publish the move command
-    rospy.loginfo("Moving Duckiebot forward...")
-    rate = rospy.Rate(10)  # 10 Hz
-    start_time = rospy.get_time()
+    turn_command = Twist2DStamped()
+    turn_command.header.stamp = rospy.Time.now()
+    turn_command.v = 0
+    turn_command.omega = turn_speed
 
-    while rospy.get_time() - start_time < time_to_move:
-        move_command.header.stamp = rospy.Time.now()  # Update the timestamp
-        pub.publish(move_command)
-        rate.sleep()
-
-    # Create a stop command
     stop_command = Twist2DStamped()
     stop_command.header.stamp = rospy.Time.now()
     stop_command.v = 0
     stop_command.omega = 0
-    pub.publish(stop_command)
-    rospy.loginfo("Duckiebot stopped.")
+
+    rate = rospy.Rate(10) 
+
+    for _ in range(4):  
+    
+        start_time = rospy.get_time()
+        while rospy.get_time() - start_time < time_to_move:
+            move_command.header.stamp = rospy.Time.now()
+            pub.publish(move_command)
+            rate.sleep()
+
+      
+        pub.publish(stop_command)
+        rospy.sleep(0.5)  
+
+        # Turn 90 degrees
+        start_time = rospy.get_time()
+        while rospy.get_time() - start_time < turn_time:
+            turn_command.header.stamp = rospy.Time.now()
+            pub.publish(turn_command)
+            rate.sleep()
+
+        pub.publish(stop_command)
+        rospy.sleep(0.5)  
+
 
 if __name__ == '__main__':
     try:
