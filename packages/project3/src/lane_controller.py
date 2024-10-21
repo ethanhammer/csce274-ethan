1cbe0e58c399c2a28d45e797fd040c9ce4335148
Ethan Hammer <ethanhammer2217@gmail.com>
2024-10-21 14:16:56 -0400

    new

--- packages/project3/src/lane_controller.py
+++ packages/project3/src/lane_controller.py
@@ -6,15 +6,15 @@ class SimpleLaneControllerNode:
     def __init__(self, node_name):
         rospy.init_node(node_name)
 
-        self.pub_car_cmd = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
-        self.sub_lane_pose = rospy.Subscriber("~lane_pose", LanePose, self.cbLanePose, queue_size=1)
+        self.pub_car_cmd = rospy.Publisher("/bigbot/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=1)
+        self.sub_lane_pose = rospy.Subscriber("/bigbot/lane_filter_node/lane_pose", LanePose, self.cbLanePose, queue_size=1)
 
-        self.kp_position = 1.0
+        self.kp_position = 3.5
         self.ki_position = 0.0
-        self.kd_position = 0.1
-        self.kp_heading = 2.0
+        self.kd_position = 0.0
+        self.kp_heading = 3.5
         self.ki_heading = 0.0
-        self.kd_heading = 0.5
+        self.kd_heading = 0.0
 
         self.integral_position = 0
         self.integral_heading = 0
@@ -39,7 +39,7 @@ class SimpleLaneControllerNode:
         phi_err = pose_msg.phi  # Heading error
 
         # Compute control outputs using PID controllers
-        v = 1.0  # Constant forward velocity
+        v = 0.5  # Constant forward velocity
         omega, self.integral_heading = self.compute_pid(phi_err, dt, self.previous_error_heading, self.integral_heading, self.kp_heading, self.ki_heading, self.kd_heading)
         speed_adjustment, self.integral_position = self.compute_pid(d_err, dt, self.previous_error_position, self.integral_position, self.kp_position, self.ki_position, self.kd_position)
         v += speed_adjustment
@@ -51,7 +51,7 @@ class SimpleLaneControllerNode:
         # Publish command
         car_cmd_msg = Twist2DStamped()
         car_cmd_msg.v = v
-        car_cmd_msg.omega = omega
+        car_cmd_msg.omega = omega * -1
         self.pub_car_cmd.publish(car_cmd_msg)
 
 if __name__ == "__main__":
